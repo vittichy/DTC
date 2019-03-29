ecd3568835f8b5b7c5976a69c05479e819f9297a
Vit Tichy <tichy@jkr.cz>
2019-03-29 09:07:09 +0100

    DirectoryInfoExtensions development

--- Dtc.IO/Extensions/DirectoryInfoExtensions.cs
+++ Dtc.IO/Extensions/DirectoryInfoExtensions.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -7,95 +6,39 @@ namespace Dtc.IO.Extensions
 {
     public static class DirectoryInfoExtensions
     {
-
-        ///// <summary>
-        ///// TODO vyhodit  
-        ///// </summary>
-        ///// <param name="directoryInfo"></param>
-        ///// <param name="extensions"></param>
-        ///// <param name="stringComparison"></param>
-        ///// <returns></returns>
-        //public static IEnumerable<FileInfo> GetFilesWithExtension(this DirectoryInfo directoryInfo,
-        //                                                          string[] extensions,
-        //                                                          StringComparison stringComparison = StringComparison.InvariantCultureIgnoreCase)
-        //{
-        //    bool ExtensionOk(FileInfo fileInfo)
-        //    {
-        //        var ok = extensions.Any(p => string.Equals(fileInfo?.Extension, p, stringComparison));
-        //        return ok;
-        //    }
-
-        //    var result = directoryInfo?
-        //                    .GetFiles()
-        //                        .Where(p => ExtensionOk(p));
-        //    return result;
-        //}
-
-
+        /// <summary>
+        /// get files by extension
+        /// </summary>
+        /// <param name="directoryInfo">DirectoryInfo</param>
+        /// <param name="extensions">set of extensions</param>
+        /// <param name="searchOption">SearchOption</param>
+        /// <returns></returns>
         public static List<FileInfo> GetFilesByExt(this DirectoryInfo directoryInfo,
                                                     List<string> extensions,
                                                     SearchOption searchOption = SearchOption.TopDirectoryOnly)
         {
-            if (directoryInfo != null)
-            {
-                // extesnion list prepared for searching
-                var preparedExtension = extensions.Select(p => $".{p.ToLower()}").ToList();
+            if (directoryInfo == null)
+                return new List<FileInfo>();
 
-                var allFiles = directoryInfo.GetFiles("*.*", searchOption).ToList();
-                var result = allFiles.Where(p => preparedExtension.Contains(p.Extension?.ToLower())).ToList();
-                return result;
-            }
-
-            return new List<FileInfo>();
+            // extesnion list prepared for searching
+            var preparedExtension = extensions.Select(p => $".{p.ToLower()}").ToList();
+            var allFiles = directoryInfo.GetFiles("*.*", searchOption).ToList();
+            var result = allFiles.Where(p => preparedExtension.Contains(p.Extension?.ToLower())).ToList();
+            return result;
         }
 
 
+        /// <summary>
+        /// get files by extension
+        /// </summary>
+        /// <param name="directoryInfo">DirectoryInfo</param>
+        /// <param name="extension">extension to search</param>
+        /// <param name="searchOption">SearchOption</param>
+        /// <returns></returns>
         public static List<FileInfo> GetFilesByExt(this DirectoryInfo directoryInfo,
                                                     string extension,
-                                                    SearchOption searchOption = SearchOption.TopDirectoryOnly)
-        {
-            return GetFilesByExt(directoryInfo, new List<string>() { extension }, searchOption);
-        }
-
-
-
-        ///// <summary>
-        ///// dohleda v adresari soubory majici jednu z extensions
-        ///// </summary>
-        ///// <param name="directoryInfo">zdrojova cesta</param>
-        ///// <param name="extensionList">seznam pripon souboru ... napr "jpg;jpeg;gif"</param>
-        ///// <param name="extensionListSeparator">oddelovac v extension listu</param>
-        ///// <param name="searchOption">SearchOption - hledani jen v adresari vs hledani i ve vsech podadresarich</param>
-        ///// <returns>Seznam dohledanych souboru</returns>
-        //public static List<FileInfo> GetFilesByExt(this DirectoryInfo directoryInfo, 
-        //                                           string extensionList, 
-        //                                           char extensionListSeparator, 
-        //                                           SearchOption searchOption = SearchOption.TopDirectoryOnly)
-        //{
-        //    if (directoryInfo != null)
-        //    {
-        //        var extensionSet = extensionList.Split(new char[] { extensionListSeparator })
-        //                                            .Select(p => "." + p.ToLower())
-        //                                                .ToList();
-
-        //        var allFiles = directoryInfo.GetFiles("*.*", searchOption).ToList();
-        //        var result = allFiles.Where(p => extensionSet.Contains(p?.Extension?.ToLower())).ToList();
-        //        return result;
-        //    }
-        //    return new List<FileInfo>();
-        //}
-
-
-
-        //public static List<FileInfo> GetFilesByExtRecursiveAllDirectories(this DirectoryInfo directoryInfo, 
-        //                                                                  string extensionList, 
-        //                                                                  char extensionListSeparator)
-        //{
-        //    return GetFilesByExt(directoryInfo, extensionList, extensionListSeparator, SearchOption.AllDirectories);
-        //}
-
-
-
+                                                    SearchOption searchOption = SearchOption.TopDirectoryOnly) 
+            => GetFilesByExt(directoryInfo, new List<string>() { extension }, searchOption);
     }
 }
 
---
+++ TestConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using Dtc.IO.Extensions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfoExtensionsTests();
+        }
+
+        private static void DirectoryInfoExtensionsTests()
+        {
+            //var d = new DirectoryInfo(@"c:\tmp\");
+
+            //var d1 = d.GetFilesByExt("jpg", SearchOption.AllDirectories);
+
+            //var CldPath = @"C:\_C1Sources\C1\Services\GlobalSystemService\model\";
+        }
+    }
+}
